d4cfd1b6210b15979f14e8e8413142e33f2e44ba
gmoralrubio <gmoralrubio@gmail.com>
2026-04-06 20:47:26 +0200

    feat:  ejemplo de uso de console en Node.js

---
+++ examples/01-hola-node/index.js
@@ -0,0 +1,10 @@
+console.log('Hola desde Node.js')
+
+// en node, el objeto global window no existe
+// console.log(window.screen)
+
+// nos da el Current Working Directory
+console.log('Directorio actual: ', process.cwd())
+
+// argumentos recibidos por el proceso de node, del objeto global
+console.log('Argumentos recibidos: ', process.argv)
