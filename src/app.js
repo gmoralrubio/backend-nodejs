9b9b6604261a81f6e28d4bc74026e0133025f6bd
gmoralrubio <gmoralrubio@gmail.com>
2026-04-13 19:39:37 +0200

    feat: setup inicial de ejs

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
-// En app.js inicializamos SOLO la app de express
-// modulo de node para obtener el directorio real de un path en concreto
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
-// importamos el modulo express
+
 import express from 'express'
 import morgan from 'morgan'
+import ejs from 'ejs' // importamos el motor de plantillas
 
-// Importamos el router
 import { pagesRouter } from './routes/pages-routes.js'
 import { utilitiesRouter } from './routes/utilities-routes.js'
 import { tasksRouter } from './routes/tasks-routes.js'
@@ -17,31 +15,29 @@ const appDir = dirname(fileURLToPath(import.meta.url)) // url del archivo actual
 console.log(appDir)
 console.log(import.meta.url) // Lleva el file//
 
-// Middleware propio de express que transforma en objeto datos de formulario
-// o query strings
+// Global middlewares
 app.use(express.urlencoded({ extended: true }))
-// Middleware para usar archivos estaticos
 app.use(express.static(join(appDir, '../public'))) // Crea la ruta a /public
-// morgan es un middleware de terceros para logs
 app.use(morgan('tiny'))
 
 // Custom middleware para todas las rutas
 app.use((req, res, next) => {
   console.log('Nueva Petición!')
-  // un middleware siempre tiene que contestar a la peticion o llamar a next
   next()
 })
 
+// Configuracion del motor de plantillas
+app.set('view engine', 'html') // Definimos el motor de vistas
+app.engine('html', ejs.renderFile) // Definimos el motor como html de ejs
+app.set('views', join(appDir, 'views')) // Definimos directorio de vistas
+
 // Routes
-// Todas las rutas que empiecen por / entran por los routers
 app.use('/', pagesRouter)
 app.use('/', utilitiesRouter)
 app.use('/tasks', tasksRouter)
 
 // Middleware para generar el plantillado html
 app.use((req, res, next) => {
-  // locals permite definir variables en cualquier parte de la peticion
-  // Estaran disponibles en todos los middlewares
   const renderHtml = res.locals.html
 
   if (!renderHtml) {
@@ -55,34 +51,37 @@ app.use((req, res, next) => {
 
   res.send(`
     <!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>${title}</title>
-      <link rel="stylesheet" href="/app.css"/>
-      <link rel="icon" href="/icon.webp" />
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    </head>
-    <body>
-      <nav class="container">
-        <a href="/">Home</a>
-        <a href="/tasks">Tasks (${pendingTasks})</a>
-        <a href="/health">Health</a>
-      </nav>
-      <h1>${title}</h1>
-      ${content}
+<html>
 
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    </body>
-    </html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Server HTTP básico</title>
+  <link rel="stylesheet" href="/app.css" />
+  <link rel="icon" href="/icon.webp" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+  <nav class="container">
+    <a href="/">Home</a>
+    <a href="/tasks">Tasks (0)</a>
+    <a href="/health">Health</a>
+  </nav>
+  <h1>Server HTTP básico</h1>
+  <p>
+    Renderizado con ejs. Este ejemplo ya respira web SSR: una ruta HTML, una lista HTML y una ruta de health
+  </p>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
   `)
 })
 
 // Handler 404
-// Si ha llegado hasta aqui es que no hay ninguna ruta que lo capture
 app.use((req, res) => {
-  // Con res.status seteamos el status de la response, por defecto es 200
   res.status(404).send('Resource not found')
 })
 
--- src/controllers/pages-controllers.js
+++ src/controllers/pages-controllers.js
@@ -2,6 +2,12 @@
 import { countPendingTasks } from '../data/tasksRepository.js'
 
 export async function homePageController(req, res, next) {
+  // Usamos el render de ejs
+  // Cualquier ruta que le pasemos a render es relativa a views
+  res.render('index.html') // ruta relativa al directorio de vistas
+
+  // TODO: Refactorizar
+  return
   res.locals.title = 'Server con Express básico'
   res.locals.pendingTasks = await countPendingTasks()
   res.locals.content =
