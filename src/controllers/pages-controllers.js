905684bf0834825d8968ace87f1d8c6d2d8a69df
gmoralrubio <gmoralrubio@gmail.com>
2026-04-13 19:52:25 +0200

    refactor: añadido renderizado dinámico en la home

--- src/controllers/pages-controllers.js
+++ src/controllers/pages-controllers.js
@@ -2,17 +2,16 @@
 import { countPendingTasks } from '../data/tasksRepository.js'
 
 export async function homePageController(req, res, next) {
+  const pendingTasks = await countPendingTasks()
   // Usamos el render de ejs
   // Cualquier ruta que le pasemos a render es relativa a views
-  res.render('index.html') // ruta relativa al directorio de vistas
+  // Como segundo parametro recibe el contexto de la plantilla
+  res.render('index.html', {
+    title: 'Servidor HTTP básico',
+    content:
+      'Este ejemplo ya respira web SSR: una ruta HTML, una lista HTML y una ruta de health',
+    pendingTasks: pendingTasks,
+  })
 
-  // TODO: Refactorizar
-  return
-  res.locals.title = 'Server con Express básico'
-  res.locals.pendingTasks = await countPendingTasks()
-  res.locals.content =
-    '<p>Este ejemplo ya respira web SSR: una ruta HTML, una lista HTML y una ruta de health</p>'
-  res.locals.html = true
-  next()
   return
 }
